6053f1bf95e08099f2d671ee4ad5b1976837e510
Naimur00 <aminaim4u@gmail.com>
2024-11-03 19:46:23 +0600

    learn: route group

--- app/(dashboard)/dashboard/users/[id]/page.tsx
+++ app/(dashboard)/dashboard/users/[id]/page.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
 
-const page = ({params}: {params: {id: number}}) => {
+const page = ({params}: {params: {id: string}}) => {
   const {id} = params;
   return (
     <div>user id: {id}</div>
