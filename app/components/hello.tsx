f93ff60e55e1e93577d7e2d7f567de4c002b7019
Naimur00 <aminaim4u@gmail.com>
2024-11-03 13:40:32 +0600

    Im learning Next JS

---
+++ app/components/hello.tsx
@@ -0,0 +1,12 @@
+"use client"
+
+const hello = () => {
+    console.log('Im Naimur')
+    return (
+        <div>
+            Hi, my name is naimur.
+        </div>
+    );
+};
+
+export default hello;
